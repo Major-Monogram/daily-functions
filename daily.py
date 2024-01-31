5f1a19dc68f56e9d1040b670f0b89f40e4de0ed7
Adyut Singh <adyutsingh567@gmail.com>
2024-01-31 18:08:50 +0530

    Fix typo in README.md

---
+++ daily.py
@@ -0,0 +1,4 @@
+def inp(b):
+    a = input("%s" % b)
+    return a 
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='daily-functions',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Adyut Singh',
+    author_email='adyutsingh567@email.com',
+    description='some daily using functions in short',
+    url='https://github.com/Major-Monogram/daily-functions.git',
+)
